107e1e8f2a4d4461189ce4875683733000b49cc5
bashar-salah <87838171+bashar-salah@users.noreply.github.com>
2021-07-23 04:14:31 +0300

    Update utiles.py

--- utiles.py
+++ utiles.py
@@ -1,24 +1,4 @@
-# from django.contrib.gis.geoip2 import GeoIP2
-# from  bs4  import BeautifulSoup
-# import requests
-# def scraping () :
-#
-#         url = 'https://myexternalip.com/'
-#         response = requests.get(url)
-#         suop = BeautifulSoup(response.text, 'html.parser')
-#         a = suop.find('textarea', {'id': 'ip'})
-#
-#         if a:
-#             a = a.text
-#             return a
 
-#
-# def get_geo(ip):
-#     g = GeoIP2()
-#     country = g.country(ip)
-#     city = g.city(ip)
-#     lat, lon = g.lat_lon(ip)
-#     return country, city, lat, lon
 def get_center_coordinates(latA, longA, latB=None, longB=None):
     cord = (latA, longA)
     if latB:
